72c82dd9cc3c72b38275683606db72d8c933de5d
Pouya Rahimpour <mpouya.rhm9981@gmail.com>
2026-05-20 03:07:56 +0330

    added template4

---
+++ template4.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define TESTCASE
+//#define TIME
+#define DEBUG
+#ifdef LOCAL
+#include "/home/pouya/dev/cp/lib/local.cpp"
+#else
+#define dbg(...)
+#define Solve solve
+#endif
+#define nl '\n'
+
+void solve([[ maybe_unused ]] int tc) {
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    
+    #ifdef TESTCASE
+    cin >> t;
+    #endif
+    for (int i=1; i<=t; i++) {
+        Solve(i);
+    }
+    return 0;
+}
+
+
